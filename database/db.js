366e1b2cb4375c48fd8bdc94ba9b4c4a9964c1da
yomzeew <yomzeew@gmail.com>
2024-07-16 18:07:48 +0100

    new update

--- database/db.js
+++ database/db.js
@@ -13,7 +13,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST, 
   user: process.env.DB_USER,   
-  password:'happychild2024', 
+  password: process.env.DB_PASSWORD, 
   database: process.env.DB_DATABASE 
 });
 
