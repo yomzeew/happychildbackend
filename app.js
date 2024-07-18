56b58467ffb2df1fbdaa01b0ad64938ddbd0b48c
yomzeew <yomzeew@gmail.com>
2024-07-18 19:54:17 +0100

    new update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 const corsOptions = {
-  origin: ["http://172.20.10.3:3000","http://172.20.10.3:3001","https://happychild-topaz.vercel.app", "http://localhost:3000","http://localhost:3001"],
+  origin: ["https://happychild-topaz.vercel.app","http://172.20.10.3:3000", "http://localhost:3000"],
 };
 app.use(session({
   secret: 'happychild12334mother',
